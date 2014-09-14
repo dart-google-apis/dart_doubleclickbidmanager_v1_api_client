6de1105fc8286a049865834e59e6d55deb3b083d
Adam Singer <financeCoding@gmail.com>
2014-09-13 18:52:17 -0700

    Automated update

--- lib/doubleclickbidmanager_v1_api_browser.dart
+++ lib/doubleclickbidmanager_v1_api_browser.dart
@@ -6,6 +6,7 @@ import 'package:google_doubleclickbidmanager_v1_api/src/browser_client.dart';
 import "package:google_doubleclickbidmanager_v1_api/doubleclickbidmanager_v1_api_client.dart";
 
 /** API for viewing and managing your reports in DoubleClick Bid Manager. */
+@deprecated
 class Doubleclickbidmanager extends Client with BrowserClient {
 
   final oauth.OAuth2 auth;
--- lib/doubleclickbidmanager_v1_api_console.dart
+++ lib/doubleclickbidmanager_v1_api_console.dart
@@ -7,6 +7,7 @@ import 'package:google_doubleclickbidmanager_v1_api/src/console_client.dart';
 import "package:google_doubleclickbidmanager_v1_api/doubleclickbidmanager_v1_api_client.dart";
 
 /** API for viewing and managing your reports in DoubleClick Bid Manager. */
+@deprecated
 class Doubleclickbidmanager extends Client with ConsoleClient {
 
   final oauth2.OAuth2Console auth;
